db65660a9fca2675f14fa694ceff7a7af60d56e2
Yan-Yi Li <liyanyi@gmail.com>
2016-02-10 15:06:10 +0000

    challenge 11 - create link

--- app/app.rb
+++ app/app.rb
@@ -2,9 +2,18 @@ require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
   end
 
   run! if app_file == $0
