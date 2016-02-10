99d8c1b8ec5866387f51bc93a854641fe9223452
Yan-Yi Li <liyanyi@gmail.com>
2016-02-10 12:51:36 +0000

    challenge 10 - can see existing links on link page

--- Gemfile
+++ Gemfile
@@ -3,7 +3,10 @@ source "https://rubygems.org"
 
 ruby '2.2.3'
 
-# gem "rails"
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+
 group :test do
   gem 'rspec'
   gem 'capybara'
---
+++ app/app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require_relative 'models/link'
+
+class BookmarkManager < Sinatra::Base
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
+end
