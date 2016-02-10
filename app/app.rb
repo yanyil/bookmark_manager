f2014d1f7cd4d63766618a42a7f32f278f4450b0
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-10 17:00:16 +0000

    Added feature to create new link

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,14 @@ class Bookmark < Sinatra::Base
     erb(:links)
   end
 
+  get '/links/new' do
+    erb(:new_link)
+  end
+
+  post '/links' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -12,3 +12,14 @@ describe 'see a list of links on the homepage' do
     # click_link('http://www.google.co.uk')
   end
 end
+
+describe 'creating links' do
+  it 'adds a new link with site address and title' do
+    visit('/')
+    click_link('Add Bookmark')
+    fill_in 'title', with: 'BBC News'
+    fill_in 'url', with: 'http://news.bbc.co.uk'
+    click_button 'Submit'
+    expect(page).to have_content('BBC News')
+   end
+ end
