1b95f30715f17c6cc09ab3e2961ab4e1e7a7c327
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-02-11 15:53:20 +0100

    added heroku env for database url

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,7 +10,7 @@ class Link
 end
 
 # DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ ENV['RACK_ENV'] }")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ ENV['RACK_ENV'] }")
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
